a03d2b2cf063cce53bb87d0b0c9b7bce54de8d13
h3yon <khykhy1006@gmail.com>
2021-09-20 01:38:03 +0900

    feat: 다양한 연관관계

---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    /**
+     * self로 매핑
+     */
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),  //내가 조인해야 되고
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID") //반대쪽에서 조인해야되는 부분
+    )
+    private List<Item> items = new ArrayList<>();
+}
