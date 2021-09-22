502ab44f4ff59229dd47032a50766a59bdbed2a8
h3yon <khykhy1006@gmail.com>
2021-09-22 23:14:04 +0900

    feat: CASCADE

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 public class Category extends BaseEntity {
 
@@ -16,7 +18,7 @@ public class Category extends BaseEntity {
     /**
      * self로 매핑
      */
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
